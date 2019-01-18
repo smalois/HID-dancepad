1e0fb23c173e3724c26f6e1a7df6543000f0e57d
smalois <smalois15@gmail.com>
2019-01-17 21:14:58 -0500

    Converted sensor values to force

--- 9panel.ino
+++ 9panel.ino
@@ -1,18 +1,33 @@
 #include <Keyboard.h>
 
-bool panelstate[9];
-int zero_state;
-char characters[9] =  {'q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'};
-int panelsens[9] =    {400, 400, 400, 400, 400, 400,400, 400, 350};  // panel sensitivity val 0 - 1024
-int zero_out_values[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-int resistor_value = 10; //kilo-ohms
-int fsrReading[9];
+// Represents a 9 panel dance pad. All 9-length data arrays represent panels in
+// left-to-right, top-to-bottom order (normal english reading order). Simulates
+// a keyboard input using the leftmost letter group of a standard keyboard
+// (qweasdzxc)
+
+typedef struct panel {
+	char key_to_send; // What key this panel represents
+
+	bool state; // 1/0 -> pressed/not
+	int current_sensor_value; 
+	int sensitivity; // Button trigger tolerance
+
+	int base_sensor_value; // Sensor value after being zeroed out.
+	int accompanying_resistor_value; // in Kohms
+} panel;
+
+bool panelstate[9]; // if the panel is pressed or not
+int panel_sensitivities[9] =    {400, 400, 400, 400, 400, 400, 400, 400, 350};  // panel sensitivity val 0 - 1024
+char characters[9] =  {'q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'}; // Characters to send on panel pressed
+int zero_out_values[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // Base sensor values (to cancel weight of the panels)
+int resistor_value = 10; // Resistor value in the circuit (see arduino schematic)
+int fsrReading[9]; // Raw reading from FSRs (analog sensors)
+int zero_state;	// Button state for zeroing-out the panels
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2, INPUT);
   Keyboard.begin();
+  pinMode(2, INPUT); // Button to zero-out the panels
 }
 
 int normalize(int raw_reading) {
@@ -47,7 +62,7 @@ void loop() {
   }
 
   for (int i = 0; i < 9; i++) {
-    if (zeroed_out(normalize(fsrReading[i]), zero_out_values[i]) > panelsens[i] && panelstate[i] == 0) {
+    if (zeroed_out(normalize(fsrReading[i]), zero_out_values[i]) > panel_sensitivities[i] && panelstate[i] == 0) {
       panelstate[i] = 1;
       Keyboard.press(characters[i]);
     }
@@ -55,12 +70,12 @@ void loop() {
     //Serial.print(zeroed_out(normalize(fsrReading[i]), zero_out_values[i]));
     //Serial.print(", ");
     
-    if (zeroed_out(normalize(fsrReading[i]), zero_out_values[i]) < panelsens[i] && panelstate[i] == 1) {
+    if (zeroed_out(normalize(fsrReading[i]), zero_out_values[i]) < panel_sensitivities[i] && panelstate[i] == 1) {
       panelstate[i] = 0;
       Keyboard.release(characters[i]);
     }
   }    
   
   //Serial.println();
-  //delay(100);
+  delay(1);  
 }
