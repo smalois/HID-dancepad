a7983704a2a0cfc0d31fbb13f36ad871a151759a
smalois <smalois15@gmail.com>
2019-01-22 20:11:35 -0500

    Converted sensor values to force values

--- src/sketch.ino
+++ src/sketch.ino
@@ -6,13 +6,14 @@
 // (qweasdzxc)
 
 typedef struct panel {
-	char key_to_send; // What key this panel represents
-
 	bool state; // 1/0 -> pressed/not
-	int sensor_value; 
-	int sensitivity; // Button trigger tolerance
 
-	int base_sensor_value; // Sensor value after being zeroed out.
+	int cur_force; 
+	int sensitivity; // Button trigger tolerance as a 
+					 // percentage of max sensor capacity
+
+	int base_force; // Sensor value after being zeroed out.
+	char key_to_send; // What key this panel represents
 	int accompanying_resistor_value; // in Kohms
 } panel;
 
@@ -25,16 +26,12 @@ void setup() {
   pinMode(2, INPUT); // Button to zero-out the panels
 }
 
-int normalize(int raw_reading) {
-  return pow((double)raw_reading/ 1024, 10) * 1024;
-}
-
 // panel sensor value to force. 
-unsigned long ptof(struct panel sensor) {
+unsigned long ptof(int sensor_val) {
 	unsigned long conductance = 1000000;
-	int voltage = map(sensor.sensor_value, 0, 1023, 0, 5000);
+	int voltage = map(sensor_val, 0, 1023, 0, 5000);
 	unsigned long fsr_resistance = 5000 - voltage;
-	fsr_resistance *= sensor.accompanying_resistor_value;;
+	fsr_resistance *= 10000;
 	fsr_resistance /= voltage;
 	conductance /= fsr_resistance;
 	
@@ -44,24 +41,27 @@ unsigned long ptof(struct panel sensor) {
 	return (conductance - 1000) / 30;
 }
 
-int zeroed_out(int sensor_val, int zero_val) {
-  int denominator = 1024-zero_val;
-  if (denominator == 0) {
-    denominator = 1;
-  }
-  return ((float)abs((sensor_val-zero_val)) / (float)denominator) * 1024;
+int zeroed_out(int cur_force, int zero_val) {
+	int denominator = ptof(1024) - zero_val;
+	if (denominator == 0) {
+		denominator = 1;
+	}
+	return ((float)abs((cur_force - zero_val)) / (float)denominator) * ptof(1024);
 }
 
-int is_pressed(struct panel cur_panel) {
-	int voltage = map(cur_panel.sensor_value, 0, 1023, 0, 5000);
-	return cur_panel.sensor_value < cur_panel.sensitivity; 
+bool is_pressed(struct panel cur_panel) {
+	unsigned long denominator = ptof(1024) - cur_panel.base_force;
+	unsigned long nominator = cur_panel.cur_force - cur_panel.base_force;
+	double percentage_pressed = nominator / denominator;
+	
+	return percentage_pressed > cur_panel.sensitivity; 
 }
 
 void loop() {
-	dancepad[1].sensor_value = analogRead(6);
-	dancepad[3].sensor_value = analogRead(7);
-	dancepad[5].sensor_value = analogRead(8);
-	dancepad[7].sensor_value = analogRead(9);
+	dancepad[1].cur_force = ptof(analogRead(6));
+	dancepad[3].cur_force = ptof(analogRead(7));
+	dancepad[5].cur_force = ptof(analogRead(8));
+	dancepad[7].cur_force = ptof(analogRead(9));
   	zero_state = digitalRead(2);
 
 	// Check the state of the zero-out button
@@ -69,7 +69,7 @@ void loop() {
 		// Zero out every panel by setting the base sensor value
 		// (Don't step on anything when pressing this button)
     	for (int i = 0; i < 9; i++) {
-      		dancepad[i].base_sensor_value = normalize(dancepad[i].sensor_value);
+      		dancepad[i].base_force = dancepad[i].cur_force;
     	}
   	}
 
