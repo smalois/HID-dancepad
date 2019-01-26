439fcc3138ee0f3e12a7edf9abee814cf55345c1
smalois <smalois15@gmail.com>
2019-01-26 18:49:31 -0500

    Added sensitivity tolerences.
    Also fixed indentation and removed some buggy code for zero-ing out the panels.

--- src/sketch.ino
+++ src/sketch.ino
@@ -6,75 +6,66 @@
 // (qweasdzxc)
 
 typedef struct panel {
-  bool state; // 1/0 -> pressed/not
+	bool state; // 1/0 -> pressed/not
 
-  int cur_force; 
-  int sensitivity; // Button trigger tolerance as a 
-           // percentage of max sensor capacity
+	int cur_force;	 // in Newtons
+	int sensitivity; // Button trigger tolerance based on force value in
+					 // Newtons. Setting these properly will likely take some
+					 // trial/error based on your sensors and how they're
+					 // mounted.
 
-  int base_force; // Sensor value after being zeroed out.
-  char key_to_send; // What key this panel represents
-  int accompanying_resistor_value; // in Kohms
+	int base_force; // Sensor value after being zeroed out.
+	char key_to_send; // What key this panel represents
+	int accompanying_resistor_value; // in Kohms
 } panel;
 
 int zero_state; // Button state for zeroing-out the panels
 panel dancepad[9];
+char keys[9] = {'q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'};
+int sensitivities[9] = {60, 60, 60, 60, 60, 60, 60, 60, 60};
+int resistors[9] = {10, 10, 10, 10, 10, 10, 10, 10, 10};
+
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(2, INPUT); // Button to zero-out the panels
-  //bashKeyboard.begin();
-  dancepad[0].key_to_send = 'q';
-  dancepad[1].key_to_send = 'w';
-  dancepad[2].key_to_send = 'e';
-  dancepad[3].key_to_send = 'a';
-  dancepad[4].key_to_send = 's';
-  dancepad[5].key_to_send = 'd';
-  dancepad[6].key_to_send = 'z';
-  dancepad[7].key_to_send = 'x';
-  dancepad[8].key_to_send = 'c';
+	Serial.begin(9600);
+	pinMode(2, INPUT); // Button to zero-out the panels
+	//Keyboard.begin();
+	// Initialize all of the panel values
+	for (int i = 0; i < 9; i++) {
+		dancepad[i].key_to_send = keys[i];
+		dancepad[i].sensitivity = sensitivities[i];
+		dancepad[i].accompanying_resistor_value = resistors[i];
+	}
 }
 
-// panel sensor value to force. 
+// Convert panel sensor value to force (N).
 unsigned long ptof(int sensor_val) {
-  unsigned long conductance = 1000000;
-  int voltage = map(sensor_val, 0, 1023, 0, 5000);
-  unsigned long fsr_resistance = 5000 - voltage;
-  fsr_resistance *= 10000;
-  fsr_resistance /= voltage;
-  conductance /= fsr_resistance;
-  
-  if (conductance <= 1000) {
-    return conductance / 80;
-  } 
-  return (conductance - 1000) / 30;
+	unsigned long conductance = 1000000;
+	int voltage = map(sensor_val, 0, 1023, 0, 5000);
+	unsigned long fsr_resistance = 5000 - voltage;
+	fsr_resistance *= 10000;
+	fsr_resistance /= voltage;
+	conductance /= fsr_resistance;
+	
+	if (conductance <= 1000) {
+		return conductance / 80;
+	} 
+	return (conductance - 1000) / 30;
 }
 
-int zeroed_out(int cur_force, int zero_val) {
-  int denominator = ptof(1024) - zero_val;
-  if (denominator == 0) {
-    denominator = 1;
-  }
-  return ((float)abs((cur_force - zero_val)) / (float)denominator) *
-ptof(1024);
-}
 
 bool is_pressed(struct panel cur_panel) {
-  unsigned long denominator = ptof(500) - cur_panel.base_force;
-  unsigned long nominator = cur_panel.cur_force - cur_panel.base_force;
-  double percentage_pressed = (double)nominator / (double)denominator;
-  Serial.println(percentage_pressed);
-  return percentage_pressed > cur_panel.sensitivity; 
+	return cur_panel.cur_force > cur_panel.sensitivity; 
 }
 
 void loop() {
-  dancepad[1].cur_force = ptof(analogRead(6));
-  dancepad[3].cur_force = ptof(analogRead(7));
-  dancepad[5].cur_force = ptof(analogRead(8));
-  dancepad[7].cur_force = ptof(analogRead(9));
+	dancepad[1].cur_force = ptof(analogRead(6));
+	dancepad[3].cur_force = ptof(analogRead(7));
+	dancepad[5].cur_force = ptof(analogRead(8));
+	dancepad[7].cur_force = ptof(analogRead(9));
     zero_state = digitalRead(2);
 
-  // Check the state of the zero-out button
+  	// Check the state of the zero-out button
     if(zero_state == HIGH) {
     // Zero out every panel by setting the base sensor value
     // (Don't step on anything when pressing this button)
@@ -83,21 +74,21 @@ void loop() {
       }
     }
 
-  // Read the state of each panel
+  	// Read the state of each panel
     for (int i = 0; i < 9; i++) {
-    if (is_pressed(dancepad[i])) {
-      dancepad[i].state = 1;
-      //Keyboard.press(dancepad[i].key_to_send);
-    }
-    
-    //Serial.print(dancepad[i].cur_force);
-    //Serial.print(", ");
-    
-    if (!is_pressed(dancepad[i])) {
-      dancepad[i].state = 0;
-      //Keyboard.release(dancepad[i].key_to_send);
-    }
-  }    
+    	if (is_pressed(dancepad[i])) {
+			dancepad[i].state = 1;
+			//Keyboard.press(dancepad[i].key_to_send);
+    	}
+    	
+    	//Serial.print(dancepad[i].cur_force);
+    	//Serial.print(", ");
+    	
+    	if (!is_pressed(dancepad[i])) {
+			dancepad[i].state = 0;
+    		//Keyboard.release(dancepad[i].key_to_send);
+    	}
+  	}    
   
     //Serial.println();
     delay(1);  
